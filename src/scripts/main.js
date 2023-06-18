e3300505d7d02775eb20cfb3930ae85cd29d351c
Remi Pince <remi.pince@outlook.com>
2023-06-18 09:34:39 +0200

    full redo of new algo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,92 +1,52 @@
-// const searchInput = document.querySelector("#search-bar-input");
-const recipeList = document.querySelector("#container-cards-recipes");
+const searchBar = document.getElementById("search-bar-input");
 
-function generateRecipeHTML(recipe) {
-  // Get the ingredients HTML
-  const ingredientsHTML = recipe.ingredients
-    .map((ingredient) => {
-      const quantity = ingredient.quantity ? ingredient.quantity + " " : "";
-      const unit = ingredient.unit ? ingredient.unit + " de " : "";
-      return `<li>${quantity}${unit}${ingredient.ingredient}</li>`;
-    })
-    .join("");
+let filterCardsByInput = [];
+let filterFurtherByTags = [];
+let filterCardsByTags = [];
 
-  // Generate the figure HTML
-  const figureHTML = `
-    <figure class="main__recipes__figure">
-      <div class="main__recipes__figure__image-placeholder"></div>
-      <figcaption class="main__recipes__figure__figcaption">
-        <h2 class="main__recipes__figure__figcaption__title">${recipe.name}</h2>
-        <p class="main__recipes__figure__figcaption__time">${recipe.time} min</p>
-        <ul class="main__recipes__figure__figcaption__ingredients">${ingredientsHTML}</ul>
-        <p class="main__recipes__figure__figcaption__description">${recipe.description}</p>
-      </figcaption>
-    </figure>
-  `;
+let ingredientsListFiltred = [];
+let appliancesListFiltred = [];
+let ustensilsListFiltred = [];
 
-  return figureHTML;
-}
-
-function searchRecipes(query) {
-  return recipes.filter((recipe) => {
-    let match = false;
-    const lowerCaseQuery = query.toLowerCase();
-
-    match = recipe.name.toLowerCase().includes(lowerCaseQuery);
-
-    if (!match) {
-      recipe.ingredients.forEach((ingredient) => {
-        if (ingredient.ingredient.toLowerCase().includes(lowerCaseQuery)) {
-          match = true;
-        }
-      });
-    }
-
-    if (!match) {
-      match = recipe.appliance.toLowerCase().includes(lowerCaseQuery);
-    }
-
-    if (!match) {
-      recipe.ustensils.forEach((utensil) => {
-        if (utensil.toLowerCase().includes(lowerCaseQuery)) {
-          match = true;
-        }
-      });
-    }
-
-    return match;
-  });
-}
-
-let timerId;
-let originalRecipes = recipes.slice();
-
-function searchInput(query) {
-  clearTimeout(timerId);
-
-  if (query.length >= 3) {
-    timerId = setTimeout(() => {
-      const filteredRecipes = searchRecipes(query);
-      recipeList.innerHTML = "";
-    }, 500);
+function noMatch(value, matches, number) {
+  if (value.length < number) {
+    return null;
   } else {
-    recipeList.innerHTML = "";
-    originalRecipes.forEach((recipe) => {
-      const recipeHTML = generateRecipeHTML(recipe);
-      recipeList.insertAdjacentHTML("beforeend", recipeHTML);
-    });
+    if (matches.length === 0) {
+      containerCards.innerHTML = "";
+    }
   }
 }
 
-recipes.sort((a, b) => a.name.localeCompare(b.name));
-recipes.forEach((recipe) => {
-  const recipeHTML = generateRecipeHTML(recipe);
-  recipeList.insertAdjacentHTML("beforeend", recipeHTML);
-});
-
-function updateDisplayedRecipes(recipes) {
-  const recipeList = document.querySelector("#recipes-container");
-  recipeList.innerHTML = "";
+function renderAllArraySFiltred(ArrayFiltred) {
+  return (containerCards.innerHTML = ""), renderRecipesCards(ArrayFiltred);
 }
 
-searchInput("coco");
+function normalizeElement(element) {
+  return element
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+}
+
+function requestBySearchBar(searchText) {
+  if (searchText.length >= 3) {
+    filterCardsByInput = allRecipes.filter((recipe) => {
+      const regex = new RegExp(normalizeElement(`${searchText}`));
+      let ingredientsArray = [];
+      for (let key in recipe.ingredients) {
+        let ingredientElts = recipe.ingredients[key].ingredient;
+        ingredientsArray.push(ingredientElts);
+      }
+      return (
+        normalizeElement(recipe.name).match(regex) ||
+        normalizeElement(`${ingredientsArray}`).match(regex) ||
+        normalizeElement(recipe.description).match(regex)
+      );
+    });
+    noMatch(searchText, filterCardsByInput, 3);
+    renderAllArraySFiltred(filterCardsByInput);
+  } else {
+    renderAllArraySFiltred(allRecipes);
+  }
+}
